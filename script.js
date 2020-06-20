cc731d9e5aeab4a71af59de73eedf912d0d0a911
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-06-20 08:50:30 +0300

    I created a swictch that sets all conditions and creted some functions of each

--- script.js
+++ script.js
@@ -23,3 +23,48 @@ let myLinks = [
     author: "Fatman"
   },
 ]
+
+// We need a new variable to ssign the menues
+const choices = `Welcome to my social links!
+1 : Show links
+2 : Add a link
+3 : Remove a link
+0 : Quite`;
+
+// Here we will create the prompt for users to enter their choice
+let choiceMenue = Number(prompt(choices));
+//Here we create a whie loop to wrap all the conditions
+while (choiceMenue !== 0) {
+  switch(choiceMenue) {
+    case 1:
+      showLinks();
+      break;
+    case 2:
+      addLinks();
+      break;
+    case 3:
+      removeLinks();
+      break;
+    case 0:
+      alert(`See you!`);
+      break;
+    default:
+      alert(`Choose between 1, 2, 3, 0`);
+  }
+  choiceMenue = Number(prompt(choices));
+}
+// Then we will declare a function that lists all the links
+function showLinks() {
+  for (let i = 0; i < myLinks.length; i++) {
+    // Here we show them one at a time
+    alert(`Title: ${myLinks[i].title} \n Url: ${myLinks[i].url} \n Author: ${myLinks[i].author}`);
+  }
+}
+
+// We will create a second function in which the user can add a new link
+function addLinks() {
+  let newTitle = prompt(`Your link title`);
+  let newUrl = prompt(`And its url`);
+  let newAthor = prompt(`Then the author`);
+}
+
