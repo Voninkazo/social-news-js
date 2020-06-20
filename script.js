48c1b64423ba6c41db87569a893a95297729a3d2
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-06-20 08:25:26 +0300

    I created my array of link objects

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 // write your code here
 console.log('it works!');
+//First, let's create an array contains some objects of links
+let myLinks = [
+  {
+    title: "Banana",
+    url: "https://banana.com",
+    author: "Guest"
+  },
+  {
+    title: "Orange",
+    url: "https://orange.com",
+    author: "somebody"
+  },
+  {
+    title: "Pinapple",
+    url: "https://pinapple.com",
+    author: "Funny"
+  },
+  {
+    title: "Apple",
+    url: "https://apple.com",
+    author: "Fatman"
+  },
+]
